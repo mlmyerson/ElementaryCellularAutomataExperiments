1d06fe7f301ff9af106af3952bcfa3230ef41508
mmyerson <mlmyerson@gmail.com>
2025-08-13 17:52:05 -0400

    the lattice sort of grows

--- main.py
+++ main.py
@@ -9,24 +9,42 @@ rule = {0: 0,
         }
 
 def neighborhood(pos, lattice):
-    out = 0
+    left = 0
+    center = 0
+    right = 0
     for pos in range(pos, 3):
-        left = False
-        if pos > -1:
-            left = bool(lattice & pos-1) != 0
-        center = bool(lattice & pos) != 0
-        right = False
-        if pos < lattice.bit_length() - 1:
-            right = bool(lattice & pos+1) != 0
+        if pos > 0:
+            left = lattice & pos-1
+        center = lattice & pos
+        right = 0
+        if pos < lattice.bit_length():
+            right = lattice & pos+1
     return (left, center, right)
         
+def buildRuleKey(left, center, right):
+    rule_key = 0b0
+    rule_key |= left << 2
+    rule_key |= center << 1
+    rule_key |= right << 0
+    return rule_key
+
+#apply rules across the lattice
+def applyRule(lattice):
+    new_lattice = 0b0
+    for pos in range(lattice.bit_length()):
+        left, right, center = neighborhood(pos, lattice)
+        rule_key = buildRuleKey(left, center, right)
+        new_bit = rule[rule_key]
+        new_lattice |= new_bit << pos
+    
+    # Add padding: shift left by 1 to append 0, then add space for prepending
+    new_lattice = new_lattice << 1  # Append 0 bit on the right
+    # Prepending 0 bit on the left is automatic since we're not setting that bit
+    
+    return new_lattice
+
+
 lattice = 0b010
-# latticeInt = int(lattice, 2)
-
-for pos in range(lattice.bit_length()):
-    left, right, center = neighborhood(pos, lattice)
-    # is_one = bool(lattice & pos) != 0
-    # if (is_one):
-
-
-print(rule)
+for _ in range(10):
+    print(f"{lattice:b}")  
+    lattice = applyRule(lattice)
